d889e06761dafff31fa1f399144fde11b7276fa9
haya4 <hayashi.yuu@gmail.com>
2022-06-25 00:11:30 +0900

    全国版

--- index.js
+++ index.js
@@ -1,88 +1,91 @@
-var debug = {};
-var site = "https://yuuhayashi.github.io/plateau_samples4import/";
+var __map;
+var __markerLayer = null;
+var __overlay = null;		// ポップアップを表示するためのレイヤー
+var site = null;
 
-var map = L.map('map',{
-  center: [35.658, 139.745],
-  zoom: 11,
-  minZoom: 6,
-  maxZoom: 16,
-  contextmenu: true,
-  contextmenuWidth: 180,
-});
+function loadMap() {
 
-//ポップアップを表示するための関数
-var popup = L.popup();
-var onGsClick = function(e) {
-    if (e.feature) {
-		return
-		    popup
-		        .setLatLng(e.latlng)
-		        .setContent(e.feature.properties["id"])
-		        .openOn(map);
-	}
-};
+	// マップの作成
+	__map = new ol.Map({
+		target: 'map',
+		layers: [
+			new ol.layer.Tile({
+				source: new ol.source.OSM()
+			})
+		],
+		view: new ol.View({
+			center: ol.proj.fromLonLat([139.745433,35.658581]),
+			zoom: 8
+		})
+	});
 
-//OSMレイヤー追加
-var osm = L.tileLayer(
-    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>',
-        maxZoom: 16
-    }
-);
-osm.addTo(map);
-// 11230_niiza-shi_2020
-/*
-var dpath11230 = "11230_niiza-shi_2020";
-$.getJSON(site+dpath11230+"/bldg/"+dpath11230+".geojson", function(data) {
-    L.geoJson(data, {
-        onEachFeature: function (feature, layer) {
-			if (feature.geometry.type === "Point") {
-				if (feature.properties.path === "undefined") {
-				    layer.bindPopup("code: "+ feature.properties.id
-				    	+"<br/>version: "+feature.properties.version);
-				}
-				else {
-				    layer.bindPopup("code: "+ feature.properties.id
-				    	+"<br/>version: "+feature.properties.version
-				    	+"<br/>path: "+feature.properties.path
-				    	+"<br/><a href='"+ site +feature.properties.path +"'>DOWNLOAD</a>");
-				}
-			}
-		}
-    }).addTo(map);
-});
-*/
-// 11230_niiza-shi_2020
-var mvt11230 = new L.TileLayer.MVTSource({
-    url: site + "tile/t11230/{z}/{x}/{y}.pbf",
-    clickableLayers: ["citygml"],	//クリックできるレイヤー名を指定する
-    mutexToggle:true,
-    getIDForLayerFeature: function(feature) {
-        return feature.properties["id"];
-    },
-    onClick: onGsClick, //地物クリック時の処理
-    style: function (feature) {
-        var style = {};
-        style.color = 'rgba(255,0,0,1)';
-        style.radius = 10;
-        style.selected = {
-          radius: 15
-        };
-        return style;
-    }
-});
-map.addLayer(mvt11230);
+    // マーカーの見た目の作成
+    var style = new ol.style.Style({
+        image: new ol.style.Icon({
+            src: 'lib/img/osm_200x200.png',
+            anchor: [0.5, 0.5],
+            scale: 0.2
+        })
+    });
+	
+    // 表示するためのレイヤーを作成する
+    __markerLayer = new ol.layer.Vector({
+        source: new ol.source.Vector()
+    });
+    __map.addLayer(__markerLayer);
 
+    // ポップアップを表示するためのオーバーレイを作成する
+    __overlay = new ol.Overlay({
+        element: document.getElementById('popup'),
+        positioning: 'bottom-center'
+    });
 
-//Globals that we can change later.
-var fillColor = 'rgba(149,139,255,0.4)';
-var strokeColor = 'rgb(20,20,20)';
+	$.when(
+		$.getJSON("index.json")
+	).done(function(data) {
+		site = data.site;
+		style = new ol.style.Style({
+		    image: new ol.style.Icon({
+		        src: site + 'lib/img/osm_200x200.png',
+		        anchor: [0.5, 0.5],
+		        scale: 0.2
+		    }),
+		    stroke: new ol.style.Stroke({color: '#ff33ff', width: 10})
+		});
 
-L.control.scale({imperial:false}).addTo(map);
-
-    // MapCenterCoord
-    var options = {
-      position: 'bottomleft' // 'topleft', 'topright', 'bottomleft' (default) ,'bottomright'
-    }
-    L.control.mapCenterCoord(options).addTo(map);
-
+	    $(data.list).each(function() {
+		    var feature = new ol.Feature({
+		        geometry: new ol.geom.Point(ol.proj.fromLonLat(this.coordinates)),
+		        code: this.code,
+		        name: this.name
+		    });
+		    feature.information = this;
+		    feature.setStyle(style);
+		    __markerLayer.getSource().addFeature(feature);
+	    });
+	    
+	    // 地図のクリックイベントを設定
+	    __map.on('click', function (evt) {
+	        var feature = __map.forEachFeatureAtPixel(
+				evt.pixel,
+	            function (feature) {
+	                return feature;
+	            }
+	        );
+	        if (feature) {
+	            var coordinates = feature.getGeometry().getCoordinates();
+	            var info = feature.information;
+	            var element = __overlay.getElement();
+	            var descriptionHTML =
+	                "<div>" + info.code + "</div>" +
+	                "<div>" + info.name + "</div>" +
+	                "<div><a href='index2.html?code="+ info.code +"'>" + info.path + "</a></div>";
+	            element.innerHTML = descriptionHTML;
+	            __overlay.setPosition(coordinates);
+	            __map.addOverlay(__overlay);
+	        } else {
+	            __map.removeOverlay(__overlay);
+	        }
+	    });
+	});
+}
